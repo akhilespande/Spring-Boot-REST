17d301a56ea2602c9ae05064a234fa6143fd9996
akhilesh <akhilespande@gmail.com>
2019-02-08 12:23:03 +0530

    spring boot with rest services

---
+++ src/main/java/com/akhilesh/spring/PersonController.java
@@ -0,0 +1,33 @@
+package com.akhilesh.spring;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PersonController {
+
+	@Autowired
+	private Person person;
+	
+	@RequestMapping("/")
+	public String healthCheck() {
+		return "Welcome Spring Boot Rest Application";
+	}
+	
+	@RequestMapping("/person/get")
+	public Person getPerson(@RequestParam(name="name", required=false, defaultValue="Unknown") String name) {
+		person.setName(name);
+		return person;
+	}
+	
+
+	@RequestMapping(value="/person/update", method=RequestMethod.POST, consumes = "application/json")
+	public Person updatePerson(@RequestBody Person p) {
+		person.setName(p.getName());
+		return person;
+	}
+}
